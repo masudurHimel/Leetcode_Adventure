f0341a9081ec10374ded234545331598d7a08a66
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-03-02 00:10:03 +0600

    Time: 6 ms (60.56%), Space: 3.3 MB (88.73%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.go
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.go
@@ -1,15 +1,12 @@
 func arraySign(nums []int) int {
-    neg_counter := 0
+    neg_flag := 1
     for i:=0;i<len(nums);i++{
         if nums[i] == 0{
             return 0
         } else if nums[i] < 0{
-            neg_counter++
+            neg_flag *= -1
         }
     }
-    if neg_counter % 2 != 0{
-        return -1
-    }
     
-    return 1
+    return neg_flag
 }
