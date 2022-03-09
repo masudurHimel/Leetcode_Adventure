d3d27f805a8c2e05e433e63fc8a5c82bb6cb7ae6
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-03-09 15:52:05 +0600

    Time: 1943 ms (5.03%), Space: 8.5 MB (86.45%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.go
+++ 217-contains-duplicate/217-contains-duplicate.go
@@ -1,10 +1,7 @@
 func containsDuplicate(nums []int) bool {
-    count := 1
     for i:=0;i<len(nums);i++{
-        count = 1
         for j:=0;j<len(nums);j++{
             if i != j && nums[i] == nums[j]{
-                count++
                 return true
             }
         }
