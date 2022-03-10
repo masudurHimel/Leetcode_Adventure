3fb31b9e273e946481c3993ab2b266f297655527
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-03-10 11:19:25 +0600

    Time: 64 ms (33.06%), Space: 7.8 MB (52.07%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.go
+++ 1603-design-parking-system/1603-design-parking-system.go
@@ -1,21 +1,32 @@
-class ParkingSystem:
+type ParkingSystem struct {
+	big int
+    medium int
+    small int
+}
 
-    def __init__(self, big: int, medium: int, small: int):
-        self.big = big
-        self.medium = medium
-        self.small = small
-        self.car_dict = {1: self.big, 2:self.medium, 3:self.small}
-        
-
-    def addCar(self, carType: int) -> bool:
-        if self.car_dict[carType] > 0:
-            self.car_dict[carType] -= 1
-            return True
-        else:
-            return False
-        
+func Constructor(big int, medium int, small int) ParkingSystem {
+    return ParkingSystem{big, medium, small}
+}
 
 
-# Your ParkingSystem object will be instantiated and called as such:
-# obj = ParkingSystem(big, medium, small)
-# param_1 = obj.addCar(carType)
+func (this *ParkingSystem) AddCar(carType int) bool {
+    if carType == 1 && this.big > 0{
+        this.big--
+        return true
+    }else if carType == 2 && this.medium > 0{
+        this.medium--
+        return true
+    }else if carType == 3 && this.small > 0{
+        this.small--
+        return true
+    }else{
+        return false
+    }
+}
+
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * obj := Constructor(big, medium, small);
+ * param_1 := obj.AddCar(carType);
+ */
