7cc0cd578f09e97f6ad5b08dff6e406e91b2688f
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-03-12 15:45:28 +0600

    Time: 40 ms (65.97%), Space: 8.7 MB (36.95%) - LeetHub

--- 189-rotate-array/189-rotate-array.go
+++ 189-rotate-array/189-rotate-array.go
@@ -1,12 +1,6 @@
 func rotate(nums []int, k int)  {
-    if len(nums) < k{
-        k = k%len(nums)
-    }
-    temp := make([]int, 0)
-    sample := make([]int, 0)
-    temp = nums[len(nums)-k:]
-    sample = nums[:len(nums)-k]
-    temp = append(temp, sample...)
+    k = k%len(nums)
+    temp := append(nums[len(nums)-k:], nums[:len(nums)-k]...)
     for i:=0;i<len(nums);i++{
         nums[i] = temp[i]
     }
