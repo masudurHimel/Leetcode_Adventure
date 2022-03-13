028e7f4aadf6b66b3ad561f8eea0aeea7c3f08bd
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-03-13 17:22:57 +0600

    Time: 127 ms (61.47%), Space: 9.5 MB (75.94%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.go
+++ 53-maximum-subarray/53-maximum-subarray.go
@@ -1,14 +1,20 @@
-class Solution:
-    def maxSubArray(self, nums):
-        max_res = nums[0]
-        res = [0] * len(nums)
-        res[0] = nums[0]
-        for i in enumerate(nums[1:]):
-            _ = i[1] + res[i[0]]
-            if _ > i[1]:
-                res[i[0]+1] = _
-            else:
-                res[i[0]+1] = i[1]
-            if max_res < res[i[0]+1]:
-                max_res = res[i[0]+1]
-        return max_res
+func maxSubArray(nums []int) int {
+    max_res := nums[0]
+    res := make([]int, len(nums))
+    res[0] = nums[0]
+    var temp int
+    
+    for i:=1;i<len(nums);i++{
+        temp = nums[i] + res[i-1]
+        if temp > nums[i]{
+            res[i] = temp
+        }else{
+            res[i] = nums[i]
+        }
+        
+        if max_res < res[i]{
+            max_res = res[i]
+        }
+    }
+    return max_res
+}
