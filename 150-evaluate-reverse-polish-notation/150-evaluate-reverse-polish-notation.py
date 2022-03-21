cd04183a7d43db60dc3ebc912c99ddb0fac47790
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-03-21 15:49:02 +0600

    Time: 198 ms (5.07%), Space: 14.3 MB (65.53%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
@@ -4,7 +4,7 @@ class Solution:
         for i in tokens:
             if i in '+-*/':
                 b, a= res.pop(), res.pop()
-                i = str(int(eval(a+i+b+'.')))
+                i = str(int(eval(a+i+b)))
             res.append(i)
         
         return int(res[0])
