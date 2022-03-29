5ea776834fea18a47cccd296451d4b836f919554
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-03-29 14:50:51 +0600

    Time: 51 ms (59.34%), Space: 14.3 MB (33.16%) - LeetHub

---
+++ 274-h-index/274-h-index.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations = sorted(citations, reverse=True)
+        
+        for i in range(len(citations)):
+            if i>=citations[i]:
+                return i
+        return len(citations)
