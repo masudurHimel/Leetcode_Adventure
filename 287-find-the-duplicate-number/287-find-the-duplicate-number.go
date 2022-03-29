9cfbbfbe7d8520686b5c836b80289ba8ba0216a8
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-03-29 12:48:18 +0600

    Time: 84 ms (91.61%), Space: 8.4 MB (87.53%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.go
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.go
@@ -1,42 +1,13 @@
-func mergeSort(items []int) []int {
-    if len(items) < 2 {
-        return items
-    }
-    first := mergeSort(items[:len(items)/2])
-    second := mergeSort(items[len(items)/2:])
-    return merge(first, second)
-}
-
-func merge(a []int, b []int) []int {
-    final := []int{}
-    i := 0
-    j := 0
-    for i < len(a) && j < len(b) {
-        if a[i] < b[j] {
-            final = append(final, a[i])
-            i++
-        } else {
-            final = append(final, b[j])
-            j++
-        }
-    }
-    for ; i < len(a); i++ {
-        final = append(final, a[i])
-    }
-    for ; j < len(b); j++ {
-        final = append(final, b[j])
-    }
-    return final
-}
-
-
-
 func findDuplicate(nums []int) int {
-    nums = mergeSort(nums)
-    for i:=0;i<len(nums)-1;i++{
-        if nums[i] == nums[i+1]{
-            return nums[i]
-        }
+    slow, fast := nums[0], nums[nums[0]]
+    for slow!=fast{
+        slow = nums[slow]
+        fast = nums[nums[fast]]
     }
-    return -1
+    slow = 0
+    for slow!=fast{
+        slow = nums[slow]
+        fast = nums[fast]
+    }
+    return slow
 }
