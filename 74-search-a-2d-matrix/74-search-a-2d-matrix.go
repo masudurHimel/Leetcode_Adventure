526b80c0742ae3488094aec4ae8d725c45c13605
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-04-21 10:43:29 +0600

    Time: 5 ms (35.14%), Space: 2.7 MB (74.49%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.go
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.go
@@ -20,21 +20,3 @@ func searchMatrix(matrix [][]int, target int) bool {
     return false
     
 }
-
-
-// for i in range(len(matrix)):
-//             if matrix[i][0] <= target:
-//                     l,r = 0,len(matrix[i])-1
-//                     while l<=r:
-//                         mid = (l+r)//2
-//                         print(mid)
-//                         if matrix[i][mid] == target:
-//                             return True
-//                         elif matrix[i][mid] < target:
-//                             l = mid+1
-//                         else:
-//                             r = mid-1
-                            
-//             elif matrix[i][0] > target:
-//                 return False
-//         return False
