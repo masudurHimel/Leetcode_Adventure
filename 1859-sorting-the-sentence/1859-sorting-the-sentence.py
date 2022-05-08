21a44e10c5125332103486339d2f8a6841712fae
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-05-08 12:21:51 +0600

    Time: 39 ms (56.23%), Space: 13.8 MB (59.86%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,13 +1,13 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        s_sen = {}
+        t_dict = {}
         s = s.split()
         res = ""
         
         for i in s:
-            s_sen[int(i[-1])] = i[:-1]
+            t_dict[int(i[-1])] = i[:-1]
             
-        for i in range(1, len(s_sen)+1):
-            res += s_sen[i] + " "
+        for i in range(1, len(t_dict)+1):
+            res += t_dict[i] + " "
         
         return res[:-1]
