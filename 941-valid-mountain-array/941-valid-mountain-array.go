29791cc64ce09a2d054fb2c01058e7390f2408d8
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-05-08 11:00:11 +0600

    Time: 29 ms (60.96%), Space: 6.7 MB (74.33%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.go
+++ 941-valid-mountain-array/941-valid-mountain-array.go
@@ -26,27 +26,4 @@ func validMountainArray(arr []int) bool {
     }
     
 }
-
-
-
-
-//         if len(arr) <= 2:
-//             return False
-        
-//         increasing, decreasing = False, False
-//         temp = arr[0]
-//         for i in arr[1:]:
-//             if i > temp and decreasing == False:
-//                 increasing = True
-//             elif i < temp and increasing == True:
-//                 decreasing = True
-            
-//             else:
-//                 return False
-            
-//             temp = i
-          
-//         if increasing and decreasing:
-//             return True
-//         return False
             
