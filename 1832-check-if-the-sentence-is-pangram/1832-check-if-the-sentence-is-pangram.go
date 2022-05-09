8028958fb7de814a5d39a515d75f911a502f5694
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-05-09 11:42:56 +0600

    Time: 3 ms (47.62%), Space: 2.4 MB (14.29%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.go
@@ -0,0 +1,8 @@
+func checkIfPangram(sentence string) bool{
+    res := make(map[string]int)
+    for i:=0;i<len(sentence);i++{
+        res[string(sentence[i])] = 1
+    }
+    
+    return len(res) == 26
+}
