b530a853056ac2c059f4952f81401849d9fa6193
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-05-10 14:05:52 +0600

    Time: 83 ms (73.56%), Space: 13.9 MB (58.66%) - LeetHub

--- 883-projection-area-of-3d-shapes/883-projection-area-of-3d-shapes.py
+++ 883-projection-area-of-3d-shapes/883-projection-area-of-3d-shapes.py
@@ -1,27 +1,10 @@
 class Solution:
     def projectionArea(self, grid: List[List[int]]) -> int:
-        temp_count = 0
-        for i in grid:
-            temp_count += i.count(0)
-            
-        x_axis = (len(grid)*len(grid)) - temp_count
-        y_axis = []
-        z_axis = []
-        for i in grid:
-            y_axis.append(max(i))
-            
-        t_matrix = [i for i in zip(*grid)]
-        t_matrix = [i[::-1] for i in t_matrix]
+        x = sum(j>0 for i in grid for j in i)
+        y = sum(max(i) for i in grid)
+        z = sum(max(s[i] for s in grid) for i in range(len(grid)))
         
-        for i in t_matrix:
-            z_axis.append(max(i))
-        
-        print(x_axis, y_axis, z_axis)
-        # area calculation
-        y_axis = sum(y_axis)
-        z_axis = sum(z_axis)
-        
-        return x_axis+y_axis+z_axis
+        return (x+y+z)
         
         
         
