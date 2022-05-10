7dc0d0b687a130b63e05c5744202f0aeaa97eb2a
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-05-10 11:47:54 +0600

    Time: 216 ms (30.60%), Space: 14.1 MB (75.97%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
@@ -1,12 +1,3 @@
 class Solution:
     def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
-        t_matrix = zip(*matrix)
-        t_matrix = list(map(list, t_matrix))
-        res = []
-        for i in matrix:
-            temp_max = min(i)
-            temp_index = i.index(temp_max)
-            col_max = max(t_matrix[temp_index])
-            if col_max == temp_max:
-                res.append(col_max)
-        return res
+        return list({min(row) for row in matrix} & {max(col) for col in zip(*matrix)})
