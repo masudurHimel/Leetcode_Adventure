584096d0b805790361ba07b18cbc5a5fd088e72b
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-05-24 09:52:02 +0600

    Time: 137 ms (52.10%), Space: 18.2 MB (49.45%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,7 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res = {}
-        
         for i in strs:
             _ = str(sorted(i))
             res[_] = res.get(_, []) + [i]
