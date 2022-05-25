1f86be48c5160ecc4dbba03ecf9801085259680f
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-05-25 10:13:56 +0600

    Time: 340 ms (16.98%), Space: 14.4 MB (24.38%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
@@ -3,10 +3,12 @@ class Solution:
         t_mat = list(zip(*mat))
         res = 0
         s_d = {}
+        f_d = {}
         for row in range(len(mat)):
             for j in range(len(mat[0])):
                 _ = s_d.get(j, sum(t_mat[j]))
-                if mat[row][j] == 1 and sum(mat[row]) == 1 and _ == 1:
+                _a = f_d.get(row, sum(mat[row]))
+                if mat[row][j] == 1 and _a == 1 and _ == 1:
                     res += 1
         return res
                     
