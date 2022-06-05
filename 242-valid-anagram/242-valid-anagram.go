e3a9d0c5e8726d62a03935a156b6616f3787ec8e
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-06-05 12:08:29 +0600

    Time: 3 ms (87.96%), Space: 2.8 MB (78.11%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.go
+++ 242-valid-anagram/242-valid-anagram.go
@@ -3,24 +3,17 @@ func isAnagram(s string, t string) bool {
         return false
     }
     
-    resMap := make(map[byte]int)
-    secMap := make(map[byte]int)
-    // var exist bool
-    // var secVal int
+    var resMap [26]int
+    
     
     for i:=0;i<len(s);i++{
-        resMap[s[i]] = 0
-        resMap[t[i]] = 0
-    }
-    
-    for i:=0;i<len(s);i++{
-        resMap[s[i]]++
-        secMap[t[i]]++
+        resMap[s[i] - 'a']++
+        resMap[t[i] - 'a']--
     }
      
     
-    for k,v := range resMap{
-        if v != secMap[k]{
+    for _,v := range resMap{
+        if v != 0{
             return false
         }
     }
