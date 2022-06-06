359a0f307946d52304c1692065808b172af79e08
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-06-06 11:54:18 +0600

    Time: 30 ms (67.75%), Space: 7.8 MB (45.17%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.go
+++ 238-product-of-array-except-self/238-product-of-array-except-self.go
@@ -1,19 +1,17 @@
 func productExceptSelf(nums []int) []int {
     x := make([]int, len(nums))
-    y := make([]int, len(nums))
     
-    x[0], y[len(nums)-1] = 1, 1
+    x[0] = 1
     
     for i:=1;i<len(nums);i++{
         x[i] = x[i-1] * nums[i-1]
     }
     
-    for i:=len(nums)-2;i>=0;i--{
-        y[i] = y[i+1] * nums[i+1]
-    }
+    temp := nums[len(nums)-1]
     
-    for i:=0;i<len(nums);i++{
-        x[i] = x[i] * y[i]
+    for i:=len(nums)-2;i>=0;i--{
+        x[i] *= temp
+        temp *= nums[i]
     }
     
     return x
