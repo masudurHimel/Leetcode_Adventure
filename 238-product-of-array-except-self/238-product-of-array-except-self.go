d8f40ca7d8c603ac35aee93c793e5dc803ba6500
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-06-06 11:47:04 +0600

    Time: 55 ms (15.60%), Space: 8.6 MB (12.81%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.go
@@ -0,0 +1,24 @@
+func productExceptSelf(nums []int) []int {
+    x := make([]int, len(nums))
+    y := make([]int, len(nums))
+    
+    x[0], y[len(nums)-1] = 1, 1
+    
+    for i:=1;i<len(nums);i++{
+        x[i] = x[i-1] * nums[i-1]
+    }
+    
+    for i:=len(nums)-2;i>=0;i--{
+        y[i] = y[i+1] * nums[i+1]
+    }
+    
+    fmt.Println(x,y)
+    
+    for i:=0;i<len(nums);i++{
+        x[i] = x[i] * y[i]
+    }
+    
+    return x
+}
+
+
