b25531def035cb7b8104ff4f05904c50549bd9bb
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-06-12 17:39:57 +0600

    Time: 53 ms (23.38%), Space: 13.8 MB (77.17%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -5,19 +5,19 @@ class Solution:
         
         
         x = {
-            '2': 'abc',
-            '3':'def',
-            '4':'ghi',
-            '5':'jkl',
-            '6':'mno',
-            '7':'pqrs',
-            '8':'tuv',
-            '9':'wxyz'
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'l'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z']
         }
         
         res_dict = []
         for i in digits:
-            res_dict.append(list(x[i]))
+            res_dict.append(x[i])
                             
         res = []
         
