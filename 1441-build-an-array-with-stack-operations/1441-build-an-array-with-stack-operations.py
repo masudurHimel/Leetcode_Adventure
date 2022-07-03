df83ddd60161d17b487feb7ad28965053d7aa122
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-07-03 16:32:45 +0600

    Time: 69 ms (10.30%), Space: 13.8 MB (97.21%) - LeetHub

---
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
@@ -0,0 +1,20 @@
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        ind = 1
+        res = []
+        for i in target:
+            # print(i, ind)
+            if i == ind:
+                ind += 1
+                res.append("Push")
+            elif i > ind:
+                diff = i-ind
+                res.append("Push")
+                while diff > 0:
+                    res.append("Pop")
+                    res.append("Push")
+                    ind += 1
+                    diff -= 1
+                ind += 1
+        
+        return res
