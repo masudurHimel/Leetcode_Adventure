59d21dc0ffc019f729add22166b5af3e5d5f3fd5
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-07-03 14:46:13 +0600

    Time: 227 ms (34.94%), Space: 14.3 MB (71.51%) - LeetHub

--- 914-x-of-a-kind-in-a-deck-of-cards/914-x-of-a-kind-in-a-deck-of-cards.py
+++ 914-x-of-a-kind-in-a-deck-of-cards/914-x-of-a-kind-in-a-deck-of-cards.py
@@ -3,17 +3,4 @@ class Solution:
         deck_hash = {}
         for i in deck:
             deck_hash[i] = deck_hash.get(i, 0) + 1
-            
-#         target_val = list(deck_hash.values())[0]
-        
-#         for i in range(2, target_val//2):
-#             if target_val%i==0:
-#                 target_val = i
-#                 break
-        gcm_list = []
-
-        for i in deck_hash.values():
-            if i < 2:
-                return False
-            gcm_list.append(i)
-        return math.gcd(*gcm_list) >= 2
+        return math.gcd(*list(deck_hash.values())) >= 2
