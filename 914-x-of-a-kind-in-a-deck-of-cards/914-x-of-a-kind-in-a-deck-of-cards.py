4448db8d37fc780d8ced3d77566dfbda9ffc8308
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-07-03 14:49:46 +0600

    Time: 243 ms (25.23%), Space: 14.2 MB (95.92%) - LeetHub

--- 914-x-of-a-kind-in-a-deck-of-cards/914-x-of-a-kind-in-a-deck-of-cards.py
+++ 914-x-of-a-kind-in-a-deck-of-cards/914-x-of-a-kind-in-a-deck-of-cards.py
@@ -1,6 +1,3 @@
 class Solution:
     def hasGroupsSizeX(self, deck: List[int]) -> bool:
-        deck_hash = {}
-        for i in deck:
-            deck_hash[i] = deck_hash.get(i, 0) + 1
-        return math.gcd(*list(deck_hash.values())) >= 2
+        return math.gcd(*list(Counter(deck).values())) >= 2
