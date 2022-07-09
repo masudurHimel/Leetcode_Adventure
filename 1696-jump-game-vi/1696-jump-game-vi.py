9cec7dd0e9b14bc28520c6a195008af3b8ed8421
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-07-09 15:15:13 +0600

    Time: 2421 ms (8.19%), Space: 33.5 MB (29.25%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxResult(self, nums: List[int], k: int) -> int:
+        N = len(nums)
+        heap = [] # (-sum, pos)
+
+        for i in range(N-1, -1, -1):
+            while heap and heap[0][1] > i + k:
+                heapq.heappop(heap)
+            maxincluding = - nums[i] + (heap[0][0] if heap else 0)
+            heapq.heappush(heap, (maxincluding, i))
+            if i == 0: 
+                return -maxincluding
