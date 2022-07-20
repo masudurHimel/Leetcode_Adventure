ddf44099b2327427830b50f84218dc179751ff93
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-07-20 14:23:02 +0600

    Time: 59 ms (78.51%), Space: 14.5 MB (34.60%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,3 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        s = Counter(s)
+        t = Counter(t)
+        if len(s) != len(t):
+            return False
+        
+        for i in s.keys():
+            if t.get(i,0) != s[i]:
+                return False
+        return True
