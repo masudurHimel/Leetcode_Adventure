e86dac49bdf821491b9d201d92e440d1b0cad444
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-07-21 12:47:20 +0600

    Time: 1657 ms (41.95%), Space: 25.1 MB (37.97%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,24 +1,10 @@
-# class Solution:
-#     def maxProfit(self, prices):
-#         min_v = 999999999999
-#         max_v = 0
-#         for i in prices:
-#             if i < min_v:
-#                 min_v = i
-#             if (i - min_v) > max_v:
-#                 max_v = i - min_v
-#         return max_v
-
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        stock_max = deque([])
-        max_price = 0
-        for i in range(len(prices)-1,-1,-1):
-            max_price = max(max_price, prices[i])
-            stock_max.appendleft(max_price)
-        
-        result = -999999
-        for i in range(len(prices)):
-            result = max(stock_max[i] - prices[i], result)
-        
-        return result
+    def maxProfit(self, prices):
+        min_v = 999999999999
+        max_v = 0
+        for i in prices:
+            if i < min_v:
+                min_v = i
+            if (i - min_v) > max_v:
+                max_v = i - min_v
+        return max_v
