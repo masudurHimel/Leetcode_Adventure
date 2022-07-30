8ed51ebf71d03b66b3bb6a01291448b3ca6eea99
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-07-30 19:37:27 +0600

    Time: 1278 ms (50.67%), Space: 23.9 MB (5.33%) - LeetHub

--- 916-word-subsets/916-word-subsets.py
+++ 916-word-subsets/916-word-subsets.py
@@ -1,16 +1,27 @@
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
-        temp = set(words1)
-        letters = {}
+        res = set()
+        neg = set()
+        
+        temp_words_1 = []
+        temp_words_2 = {}
+        
+        for i in words1:
+            temp_words_1.append(Counter(i))
+        
         for i in words2:
             for j in i:
                 c = i.count(j)
-                if j not in letters or c > letters.get(j):
-                    letters[j] = c
+                if j not in temp_words_2 or c > temp_words_2.get(j):
+                    temp_words_2[j] = c
         
-        for i in range(len(words1)):
-            for j in letters.keys():
-                if words1[i].count(j) < letters[j]:
-                    temp.remove(words1[i])
-                    break
-        return list(temp)
+        
+        for i in temp_words_2.keys():
+            for j in range(len(words1)):
+                if temp_words_2[i] <= temp_words_1[j].get(i, 0):
+                    res.add(words1[j])
+                else:
+                    neg.add(words1[j])
+        return res-neg
+        # print(neg)
+            
