5e41b137ff519d8520a4de9d74beaff442a3ebe1
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-08-01 15:03:44 +0600

    Time: 705 ms (12.95%), Space: 19.7 MB (16.84%) - LeetHub

--- 933-number-of-recent-calls/933-number-of-recent-calls.py
+++ 933-number-of-recent-calls/933-number-of-recent-calls.py
@@ -2,12 +2,10 @@ class RecentCounter:
 
     def __init__(self):
         self.request = []
-        self.range = []
         
 
     def ping(self, t: int) -> int:
         self.request.append(t)
-        self.range = [t-3000, t]
         ind = bisect_left(self.request, t-3000)
         return len(self.request[ind:])
 
