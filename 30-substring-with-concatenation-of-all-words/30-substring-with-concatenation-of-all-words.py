8ffba1715d164e5459d843088485061c361bcac1
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-08-13 14:58:01 +0600

    Time: 527 ms (62.39%), Space: 14.2 MB (77.75%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        substring_len = len(words) * len(words[0])
+        word_map = Counter(words)
+        
+        def helper(i):
+            word_counter = word_map.copy()
+            word_used = 0
+            
+            for j in range(i, i+substring_len, len(words[0])):
+                target = s[j:j+len(words[0])]
+                if word_counter[target] > 0:
+                    word_counter[target] -= 1
+                    word_used += 1
+                else:
+                    break
+            return word_used == len(words)
+        
+        res = []
+        for i in range(len(s)-substring_len+1):
+            if helper(i):
+                res.append(i)
+        return res
+            
