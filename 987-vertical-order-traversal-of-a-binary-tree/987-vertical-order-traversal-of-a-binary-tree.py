6a50b015c31eae1ba9887cdb15c81404dd8c52ba
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-09-04 11:18:33 +0600

    Time: 84 ms (5.45%), Space: 14.8 MB (5.42%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
@@ -6,16 +6,12 @@
 #         self.right = right
 from sortedcontainers import SortedList
 class Solution:
-    # res = None
     max_flag = None
     min_flag = None
     
     def dfs(self, node, coord):
             if not node:
                 return
-            
-            # self.res[coord[1]].append(node.val)
-            # self.row[coord[0]].append(node.val)
             self.tt[f'{coord[0]}|{coord[1]}'].add(node.val)
             
             
@@ -26,23 +22,19 @@ class Solution:
             self.dfs(node.right, (coord[0]+1, coord[1]+1))
             
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        # self.res = defaultdict(list)
-        # self.row = defaultdict(list)
         self.tt = defaultdict(SortedList)
         self.max_flag = -math.inf
         self.min_flag = math.inf
+        ans = defaultdict(list)
+        res = []
 
         self.dfs(root, (0,0))
-        self.tt = sorted(self.tt.items(), key=lambda k: int(k[0].split('|')[0]))
-        self.tt = dict(self.tt)
-        # print(self.tt)
-        ans = defaultdict(list)
+        self.tt = dict(sorted(self.tt.items(), key=lambda k: int(k[0].split('|')[0])))
+        
         for i, v in self.tt.items():
             key = i.split('|')[-1]
             ans[int(key)] += list(v)
             
-        
-        res = []
         for i in range(self.min_flag, self.max_flag+1):
             if val := ans.get(i):
                     res.append(val)
