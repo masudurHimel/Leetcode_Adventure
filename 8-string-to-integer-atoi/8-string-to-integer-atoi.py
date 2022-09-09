56eee6f142e70446e89e535cf5bc8916a1a06da1
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-09-10 00:58:56 +0600

    Time: 66 ms (26.76%), Space: 14 MB (28.68%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -1,34 +1,26 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
         sc = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-'}
+        scu = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+        
         res = ""
         for i in s:
             if i == ' ' and res == '':
                 continue
             if i in sc:
                 if res == '':
-                    sc.remove('+')
-                    sc.remove('-')
+                    sc = scu
                 res += i
             else:
                 break
-        
-        # print(res)
-        # return 0
-        
+
         if not res:
             return 0
         
         if len(res) <= 1 and res[0] in {'+', '-'}:
             return 0
         
-#         if len(res) > 1 and res[1] in {'+', '-'}:
-#             return 0
-        
         if (-2**31) < int(res) < (2**31-1):
             return int(res)
         
-        if res[0] == '-':
-            return -2**31
-        else:
-            return 2**31 -1
+        return -2**31 if res[0] == '-' else 2**31-1
