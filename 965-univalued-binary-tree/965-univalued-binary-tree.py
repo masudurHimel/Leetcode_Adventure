6411a730ff20bd0f0c62e6a61d8dbd395766d05f
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-09-15 15:12:02 +0600

    Time: 39 ms (79.65%), Space: 13.9 MB (18.53%) - LeetHub

--- 965-univalued-binary-tree/965-univalued-binary-tree.py
+++ 965-univalued-binary-tree/965-univalued-binary-tree.py
@@ -5,17 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def dfs(self,node, target_val):
-        if not node:
-            return True
-        
-        if target_val != node.val:
-            return False
-        if not self.dfs(node.left, target_val):
-            return False
-        if not self.dfs(node.right, target_val):
-            return False  
-        return True
-        
     def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
-        return self.dfs(root, root.val)
+        stack = [root]
+        temp = []
+        while stack:
+            temp = []
+            while stack:
+                node = stack.pop()
+                if node.val != root.val:
+                    return False
+                if node.right:
+                    temp.append(node.right)
+                if node.left:
+                    temp.append(node.left)
+                    
+            stack += temp
+        return True
+                
