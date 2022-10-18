2fdc9b958404b944e839ea21d25c4b7a5bef3194
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-10-18 16:03:08 +0600

    Time: 64 ms (14.84%), Space: 13.8 MB (71.72%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -0,0 +1,26 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        if not nums:
+            return nums
+        
+        res = []
+        s = nums[0]
+        e = nums[0]
+        
+        for ind, i in enumerate(nums[1:]):
+            if i == e + 1:
+                e = i
+            else:
+                if s == e:
+                    res.append(str(s))
+                else:
+                    res.append(f"{s}->{e}")
+                s = i
+                e = i  
+                # if ind + 2 >= len(nums):
+                #     res.append(str(s))
+        if s != e:
+            res.append(f"{s}->{e}")
+        else:
+            res.append(str(s))
+        return res
