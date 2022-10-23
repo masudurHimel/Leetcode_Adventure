928d3cae22e367407d599ea6f8fee46b1e1c50f3
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-10-23 22:40:35 +0600

    Time: 382 ms (29.12%), Space: 15.3 MB (48.76%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,22 +1,12 @@
 class Solution:
-    def sumMaker(self,nums):
-        nums = [0] + nums
-        for i in range(1,len(nums)):
-            nums[i] += nums[i-1]
-        return nums
-    
     def pivotIndex(self, nums: List[int]) -> int:
-        primary_nums = self.sumMaker(nums)
-        secondary_nums = self.sumMaker(nums[::-1])
+        for i in range(1, len(nums)):
+            nums[i] += nums[i-1]
         
-        p_f, s_f = 1, len(nums)
-        # print(primary_nums, secondary_nums)
-        while p_f <= len(nums):
-            # print(p_f, s_f)
-            if primary_nums[p_f-1] == secondary_nums[s_f-1]:
-                return p_f-1
-            else:
-                p_f += 1
-                s_f -= 1
+        nums = [0] + nums + [0]
         
+        for i in range(1, len(nums)-1):
+            if nums[i-1] == nums[-2] - nums[i]:
+                return i - 1
         return -1
+            
