137c4bcb428f1eb6440e4e9e3263f4a1f106010f
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-10-24 09:19:24 +0600

    Time: 209 ms (5.00%), Space: 16.9 MB (5.01%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_hash = {}
+        t_hash = {}
+        
+        for k, i in enumerate(s):
+            s_hash[i] = s_hash.get(i, []) + [k]
+        
+        for k, i in enumerate(t):
+            t_hash[i] = t_hash.get(i, []) + [k]
+            
+        return list(s_hash.values()) == list(t_hash.values())
