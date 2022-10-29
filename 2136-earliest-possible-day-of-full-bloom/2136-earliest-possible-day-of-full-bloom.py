11aefe0a5158d71fbb4efb6a4589d9cce3ee18be
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-10-29 14:02:29 +0600

    Time: 8529 ms (5.16%), Space: 36.5 MB (23.55%) - LeetHub

---
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.py
@@ -0,0 +1,16 @@
+class Solution:
+    def earliestFullBloom(self, plantTime, growTime):
+        def compare(a, b):
+            p1, g1 = a
+            p2, g2 = b
+            return (p1 + max(p2+g2, g1)) - (p2 + max(p1+g1, g2))
+        
+        plant_grow_time = list(zip(plantTime, growTime))
+        plant_grow_time.sort(key=cmp_to_key(compare))
+        
+        latest = 0
+        current = 0
+        for p, g in plant_grow_time:
+            latest = max(latest, p+g+current)
+            current+=p
+        return latest
