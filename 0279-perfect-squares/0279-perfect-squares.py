40293f24c9305f0bd956baeb20fcc4233406157a
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-11-22 14:59:49 +0600

    Time: 4093 ms (44.17%), Space: 14 MB (71.48%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,12 +1,12 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        squares = [x**2 for x in range(0, n) if x**2<=n]
-        sqs = [n]*(n+1)
-        sqs[0] = 0 
+        dp = [x**2 for x in range(0, n) if x**2<=n]
+        res = [n]*(n+1)
+        res[0] = 0 
         for i in range(1, n+1):
-            for sq in squares: 
+            for sq in dp: 
                 if i - sq < 0: 
                     break
-                if 1 + sqs[i-sq] < sqs[i]:
-                    sqs[i] = 1 + sqs[i-sq]    
-        return sqs[n]
+                if 1 +  res[i-sq] <  res[i]:
+                    res[i] = 1 +    res[i-sq]    
+        return  res[n]
