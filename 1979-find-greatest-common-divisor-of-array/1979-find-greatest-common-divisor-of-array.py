2dd5d74d005b6d969c87b9b16ea2aad757bcb38f
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-12-13 16:12:28 +0600

    Time: 127 ms (37.90%), Space: 14 MB (42.38%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.py
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.py
@@ -1,3 +1,9 @@
 class Solution:
+    @staticmethod
+    def gcd(x, y):
+        while(y):
+            x, y = y, x % y
+        return abs(x)
+    
     def findGCD(self, nums: List[int]) -> int:
-        return math.gcd(min(nums), max(nums))
+        return self.gcd(min(nums), max(nums))
