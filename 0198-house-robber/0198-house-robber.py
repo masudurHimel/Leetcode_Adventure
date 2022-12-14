6a918e4ce5c02d315005a31eb685bde8bcf68eed
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-12-14 09:02:10 +0600

    Time: 41 ms (78.32%), Space: 13.8 MB (66.57%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums):
+        if len(nums) <=1:
+            return nums[0]
+        res = [0] * len(nums)
+        res[0], res[1] = nums[0], nums[1]
+
+        for i in enumerate(nums[2:]):
+            max_val = 0
+            for j in res[:2 + i[0] - 1]:
+                if j >= max_val:
+                    max_val = j
+            res[2 + i[0]] = max_val + i[1]
+        return max(res)
