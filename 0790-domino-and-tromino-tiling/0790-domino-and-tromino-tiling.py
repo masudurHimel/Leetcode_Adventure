e1a2cc21776264cb54fa6e5098dea8aeb58668e1
Md. Masudur Rahman <masudurhimel@gmail.com>
2022-12-24 14:33:07 +0600

    Time: 40 ms (85.40%), Space: 13.8 MB (85.40%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numTilings(self, n: int) -> int:
+        dp=[0]*(n+3)
+        dp[1]=1
+        dp[2]=2
+        dp[3]=5
+        for i in range(4,n+1):
+            dp[i]=int((2*dp[i-1]+dp[i-3])%(1e9+7))
+        return dp[n]
