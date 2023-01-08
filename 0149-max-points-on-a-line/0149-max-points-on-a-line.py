1f250defbf2615f8c67fd1c15741a6239c87a36c
Md. Masudur Rahman <masudurhimel@gmail.com>
2023-01-08 10:54:26 +0600

    Time: 88 ms (88.58%), Space: 13.8 MB (94.26%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -12,9 +12,11 @@ class Solution:
             return len(points)
         
         ans = 1
-        for i, p1 in enumerate(points):
+        for i in range(len(points)):
             slopes = defaultdict(int)
-            for j, p2 in enumerate(points[i+1:]):
+            p1 = points[i]
+            for j in range(i+1, len(points)):
+                p2 = points[j]
                 slope = get_slope(p1, p2)
                 slopes[slope] += 1
                 ans = max(slopes[slope], ans)
