6e4c9da17b07bdf9551a7e3d7fa6dc362e5f638e
Md. Masudur Rahman <masudurhimel@gmail.com>
2023-02-07 09:13:54 +0600

    Time: 834 ms (93.96%), Space: 20.2 MB (51.27%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,12 @@
+class Solution:
+    def totalFruit(self, fruits):
+        count = dict()
+        i = 0
+        for j, v in enumerate(fruits):
+            count[v] = count.get(v, 0) + 1
+            if len(count) > 2:
+                count[fruits[i]] -= 1
+                if count[fruits[i]] == 0:
+                    del count[fruits[i]]
+                i += 1
+        return j - i + 1
