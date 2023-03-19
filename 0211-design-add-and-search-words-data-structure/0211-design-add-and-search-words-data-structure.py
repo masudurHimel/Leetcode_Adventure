78ec59c8405f149fda542f9fcee1a41c7694a0fe
Md. Masudur Rahman <masudurhimel@gmail.com>
2023-03-19 09:31:13 +0600

    Time: 3382 ms (93.36%), Space: 21.9 MB (98.37%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -13,7 +13,6 @@ class WordDictionary(object):
         if '.' not in word:
             return word in self.word_dict[len(word)]
         for v in self.word_dict[len(word)]:
-            # match xx.xx.x with yyyyyyy
             for i, ch in enumerate(word):
                 if ch != v[i] and ch != '.':
                     break
