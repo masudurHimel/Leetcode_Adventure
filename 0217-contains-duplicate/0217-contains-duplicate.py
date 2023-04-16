2d6f13f694bbaef0d449dc6fef1547f6b7351cfe
Md. Masudur Rahman <masudurhimel@gmail.com>
2023-04-16 21:54:06 +0600

    Time: 566 ms (49.40%), Space: 31.4 MB (13.88%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,16 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        pChecker = 0
-        nChecker = 0
+        c = {}
         for i in nums:
-            if i >= 0:
-                if (pChecker & 1<<i)>0:
-                    return True
-                pChecker |= (1 << i)
-
-            if i<0:
-                i = abs(i)
-                if(nChecker & 1<<i)>0:
-                    return True
-                nChecker |= (1<<i)
+            c[i] = c.get(i, 0) + 1
+            if c.get(i) > 1:
+                return True
         return False
