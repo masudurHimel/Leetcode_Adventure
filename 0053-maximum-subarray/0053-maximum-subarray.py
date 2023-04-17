94094730cea63f707ba01aa1d45058b3ab80a70f
Md. Masudur Rahman <masudurhimel@gmail.com>
2023-04-17 23:16:03 +0600

    Time: 759 ms (51.69%), Space: 28.6 MB (45.03%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,10 +1,8 @@
 class Solution:
     def maxSubArray(self, nums):
-        # combined = [nums[-1]] * len(nums)
         greedy = [nums[-1]] * len(nums)
 
         for i in range(len(nums) - 2, -1, -1):
-            # combined[i] = combined[i+1]+nums[i]
             greedy[i] = max(nums[i], greedy[i+1]+nums[i])
 
         return max(greedy)
