319537d6931b584a88e14566c5775fffb89c8f8c
Md. Masudur Rahman <masudurhimel@gmail.com>
2023-04-23 22:28:58 +0600

    Time: 1533 ms (68.49%), Space: 173.7 MB (20.55%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.py
+++ 1416-restore-the-array/1416-restore-the-array.py
@@ -2,7 +2,6 @@ class Solution:
     def numberOfArrays(self, s: str, k: int) -> int:
         MOD = 10**9+7
 
-        # return the number of possible ways from i to len(s)
         @cache
         def dfs(i: int) -> int:
             if i == len(s):
@@ -17,8 +16,6 @@ class Solution:
             ways = 0
             num = 0
             for l in range(i, len(s)):
-                # using int(s[i:l+1]) results in TLE;
-                # it is inefficient because there are exponentially duplicated computations
                 num = num*10 + int(s[l])
                 if num > k:
                     break
