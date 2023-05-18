bf5306d202edd6e63b6916049b223cb2de551d64
Md. Masudur Rahman <masudurhimel@gmail.com>
2023-05-18 09:26:32 +0600

    Time: 1168 ms (65.91%), Space: 55.1 MB (16.24%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
@@ -1,12 +1,12 @@
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
-        indegree = [0] * n
+        graph_tracker = [0] * n
         for edge in edges:
-            indegree[edge[1]] += 1
-        ans = []
+            graph_tracker[edge[1]] += 1
+        res = []
         
         for i in range(n):
-            if indegree[i] == 0:
-                ans.append(i)
+            if graph_tracker[i] == 0:
+                res.append(i)
                 
-        return ans
+        return res
