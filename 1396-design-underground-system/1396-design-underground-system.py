7849a04e346fb7e3aff4fd9cf9488ad48faf1d80
Md. Masudur Rahman <masudurhimel@gmail.com>
2023-05-31 09:24:22 +0600

    Time: 256 ms (33.60%), Space: 26.7 MB (25.71%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -3,20 +3,17 @@ class UndergroundSystem:
     def __init__(self):
         self.id = []
         self.duration_dict = {}
-        self.flag_dict = {}
         self.res_dict = {}
         self.travel_count = {}
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
         self.duration_dict[id] = [(stationName, t)]
-        self.flag_dict[id] = 1
         
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
         start_station, end_station = self.duration_dict[id][0][0], stationName
         start_time, end_time = self.duration_dict[id][0][1], t
         self.duration_dict[id] = self.duration_dict[id].append((stationName, t))
-        self.flag_dict[id] = 0
         travel_count = self.travel_count[f"{start_station}-{end_station}"] = self.travel_count.get(f"{start_station}-{end_station}", 0) + 1
         avg_time = (self.res_dict.get(f"{start_station}-{end_station}", 0.0) * (travel_count-1) + (end_time-start_time)) / travel_count
         self.res_dict[f"{start_station}-{end_station}"] = avg_time
