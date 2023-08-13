e694444604e61992a0b7a13d70cb6199f92c5d05
Md. Masudur Rahman <masudurhimel@gmail.com>
2023-08-13 10:02:36 +0600

    Time: 905 ms (89.23%), Space: 31.9 MB (53.85%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -5,22 +5,17 @@ class Solution:
         dp[n] = True
         
         for i in range(n-2, -1, -1):
-            # we can only check next 2 elements (first valid partition condition)
             if i == n-2:
                 dp[i] = nums[i] == nums[i + 1]
                 continue
                 
-            # we must check all 3 given conditions
             
-            # 1.
             dp[i] = nums[i] == nums[i+1] and dp[i+2]
             if dp[i]: continue
             
-            # 2.
             dp[i] = nums[i] == nums[i+1] == nums[i+2] and dp[i+3]
             if dp[i]: continue
                 
-			#3.	
             dp[i] = nums[i] + 1 == nums[i+1] and nums[i+1] + 1 == nums[i+2] and dp[i+3]
         
         return dp[0]
