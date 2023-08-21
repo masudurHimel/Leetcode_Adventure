0158b6e0a892028bc355338b819161d1940c17e9
Md. Masudur Rahman <masudurhimel@gmail.com>
2023-08-21 17:00:17 +0600

    Time: 303 ms (20.08%), Space: 21.6 MB (37.81%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -0,0 +1,14 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.request = []
+        
+
+    def ping(self, t: int) -> int:
+        self.request.append(t)
+        ind = bisect_left(self.request, t-3000)
+        return len(self.request[ind:])
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
