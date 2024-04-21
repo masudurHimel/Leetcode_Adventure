8ae4f5a76b12a5afa8943948125db5e07766979e
Md. Masudur Rahman <masudurhimel@gmail.com>
2024-04-21 16:22:59 +0600

    Time: 135 ms (39.74%), Space: 17 MB (33.44%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,28 +1,39 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        if len(flowerbed) == 1:
-            if n == 1 and flowerbed[0] == 0:
-                return True
-            elif n == 0:
-                return True
-            else:
-                return False
-        for i, v in enumerate(flowerbed):
-            if v == 0:
-                if i == 0:
-                    if flowerbed[i+1] == 0:
-                        flowerbed[i] = 1 
-                        n -= 1
-                elif i == len(flowerbed) - 1:
-                    if flowerbed[i-1] == 0:
-                        flowerbed[i] = 1
-                        n -= 1
-                else:
-                    if flowerbed[i-1] == 0 and flowerbed[i+1] == 0:
-                        flowerbed[i] = 1
-                        n -= 1
-            if n <= 0:
-                break
-        return n <= 0
-            
+        f = False
         
+        if n == 0:
+            return True
+        
+        # if flowerbed[0] != 1 and flowerbed[1] == 0:
+        #     n -= 1
+        # else:
+        #     f = False
+        
+        
+            
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 1:
+                f = True
+                continue
+            elif f:
+                f = False
+                continue
+            elif i == len(flowerbed) - 1 and flowerbed[i] != 1 and not f:
+                n -= 1
+                f = True
+            elif flowerbed[i+1] != 1:
+                n -= 1
+                f = True
+            
+            if n == 0:
+                return True
+        
+        # if flowerbed[-1] == 0 and len(flowerbed) > 1 and not f:
+        #     n -= 1
+        print(n)
+        if n:
+            return False
+        return True
+                
+            
