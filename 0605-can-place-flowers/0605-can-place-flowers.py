84db6bf1ede1b91e553f9b519bb63e684c0211e3
Md. Masudur Rahman <masudurhimel@gmail.com>
2024-04-21 16:24:32 +0600

    Time: 137 ms (30.32%), Space: 16.9 MB (72.60%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -4,21 +4,12 @@ class Solution:
         
         if n == 0:
             return True
-        
-        # if flowerbed[0] != 1 and flowerbed[1] == 0:
-        #     n -= 1
-        # else:
-        #     f = False
-        
-        
-            
+                
         for i in range(len(flowerbed)):
             if flowerbed[i] == 1:
                 f = True
-                continue
             elif f:
                 f = False
-                continue
             elif i == len(flowerbed) - 1 and flowerbed[i] != 1 and not f:
                 n -= 1
                 f = True
@@ -28,10 +19,7 @@ class Solution:
             
             if n == 0:
                 return True
-        
-        # if flowerbed[-1] == 0 and len(flowerbed) > 1 and not f:
-        #     n -= 1
-        print(n)
+
         if n:
             return False
         return True
